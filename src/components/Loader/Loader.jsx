eb68bfc21b5e88bd0b7cc59b38c5bb9a27da4e56
PetroKasperskyi <kaspersky28061993@gmail.com>
2023-09-05 12:15:08 +0300

    new loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,11 @@
-import { InfinitySpin } from 'react-loader-spinner';
+import { MagnifyingGlass } from 'react-loader-spinner';
 import { Spinner } from './Loader.styled';
 
 export const Loader = () => {
   return (
     <>
       <Spinner>
-        <InfinitySpin width="200" color="#800080" />
+        <MagnifyingGlass width="200" color="#800080" />
       </Spinner>
     </>
   );
